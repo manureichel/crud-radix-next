9b3d8211c7c212028b042cd91f266caa1e912df7
Manuel Reichel <manuelreichel@gmail.com>
2023-11-18 21:56:23 -0300

    Agrego Next-Auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "usuario@dominio.com" },
+        password: { label: "Password", type: "password", placeholder: "*******" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: '/auth/login',
+  }
+})
+
+export { handler as GET, handler as POST }
